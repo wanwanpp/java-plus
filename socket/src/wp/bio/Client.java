07821d667c3a6071a515b3e200ce35a91d919b44
王萍 <wang.03.25@qq.com>
2017-12-02 22:09:14 +0800

    给socket下的bio添加客户端认证功能

--- socket/src/wp/bio/Client.java
+++ socket/src/wp/bio/Client.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
 
@@ -21,17 +22,28 @@ public class Client {
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new PrintWriter(socket.getOutputStream(), true);
 
+            String userinfo = "";
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("请输入用户名：");
+            userinfo += scanner.next();
+            userinfo += ":";
+            System.out.println("请输入密码：");
+            userinfo += scanner.next();
+
+            out.println(userinfo);
             //向服务器端发送数据
             //每进行一次out.println，就会收到一次服务器的响应。
-            out.println("接收到客户端的请求数据...");
-            System.out.println("Client: " +  in.readLine());
+//            out.println("接收到客户端的请求数据...");
+//            System.out.println("Client: " + in.readLine());
+//
+//            out.println("接收到客户端的请求数据1111...");
+//            System.out.println("Client: " + in.readLine());
+//
+//            Thread.sleep(3000);
+//            out.println("接收到客户端的请求数据...");
+//            out.println("接收到客户端的请求数据1111...");
 
-            out.println("接收到客户端的请求数据1111...");
-            System.out.println("Client: " +  in.readLine());
-
-            Thread.sleep(3000);
-            out.println("接收到客户端的请求数据...");
-            out.println("接收到客户端的请求数据1111...");
             String response = in.readLine();
             System.out.println("Client: " + response);
 
--- socket/src/wp/bio/ServerHandler.java
+++ socket/src/wp/bio/ServerHandler.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class ServerHandler implements Runnable {
 
@@ -12,6 +13,12 @@ public class ServerHandler implements Runnable {
 
     public ServerHandler(Socket socket) {
         this.socket = socket;
+    }
+
+    HashMap<String, String> users = new HashMap<>();
+
+    {
+        users.put("wanwanpp", "980325");
     }
 
     @Override
@@ -22,12 +29,30 @@ public class ServerHandler implements Runnable {
             in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
             out = new PrintWriter(this.socket.getOutputStream(), true);
             String body = null;
-            while (true) {
-                body = in.readLine();
-                if (body == null) break;
-                System.out.println("Server :" + body);
-                out.println("服务器端回送响的应数据.");
+
+            body = in.readLine();
+            //       wanwanpp:980325
+            String[] nameAndPwd = body.split(":");
+            String user = nameAndPwd[0];
+            String pwd = nameAndPwd[1];
+            boolean hasUser = users.containsKey(user);
+            if (!hasUser) {
+                out.println("服务器：认证失败，不存在用户名为“" + user + "“的用户。");
+            } else {
+                String pwdFromDb = users.get(user);
+                if (pwdFromDb.equals(pwd)) {
+                    out.println("服务器：认证成功。");
+                } else {
+                    out.println("服务器：您的密码输入错误。");
+                }
             }
+            System.out.println("完成认证，用户名为："+user+",密码为："+pwd);
+//            while (true) {
+//                body = in.readLine();
+//                if (body == null) break;
+//                System.out.println("Server :" + body);
+//                out.println("服务器端回送响的应数据.");
+//            }
 
         } catch (Exception e) {
             e.printStackTrace();
